33b98e4312755067327d2ba6ec871262e46dcf2c
Dag-da <sylvain.leflon@gmail.com>
2022-06-16 15:47:09 +0200

    script

--- js/script.js
+++ js/script.js
@@ -1,1 +1,22 @@
-let root = document.querySelector("#root");
+let boot = document.getElementById("buton");
+const task = [];
+
+boot.onclick = function (el) {
+  let x = document.getElementById("myText").value;
+  task.push(x);
+  for (let i = 0; i < task.length; i++) {
+    const element = task[i];
+    el += `<li>${element}</li>`;
+  }
+  document.getElementById("root").innerHTML = el;
+};
+
+// buton.onclick = function () {
+//   onput = document.querySelector("#input").value;
+//   boot.innerhtml = `
+//   <li>${onput}</li>
+//   `;
+// };
+// boot.innerHTML = `
+// <li>dsgfrg</li>
+// `;
