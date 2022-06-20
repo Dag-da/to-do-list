ed579c91ec74d74ddbfe8b9b4a7801bfae18be7c
Dag-da <sylvain.leflon@gmail.com>
2022-06-20 10:57:53 +0200

    delete + done function

--- js/script.js
+++ js/script.js
@@ -1,22 +1,42 @@
-let boot = document.getElementById("buton");
-const task = [];
-
-boot.onclick = function (el) {
-  let x = document.getElementById("myText").value;
-  task.push(x);
-  for (let i = 0; i < task.length; i++) {
-    const element = task[i];
-    el += `<li>${element}</li>`;
-  }
-  document.getElementById("root").innerHTML = el;
+let boot = document.getElementById("push");
+const task = {
+  txt: "",
+  nu: 1,
 };
 
-// buton.onclick = function () {
-//   onput = document.querySelector("#input").value;
-//   boot.innerhtml = `
-//   <li>${onput}</li>
-//   `;
-// };
-// boot.innerHTML = `
-// <li>dsgfrg</li>
-// `;
+boot.onclick = function () {
+  let el, x;
+  x = document.getElementById("todo-input").value;
+  if (x == "") {
+    alert("Rajouter une tâche");
+  } else if (x.length <= 5) {
+    alert("La tâche est trop courte (min = 5 caractères)");
+  } else if (x.length >= 25) {
+    alert("La tâche est trop longue (max = 25 caractères)");
+  } else {
+    console.log("Tout est ok pour envoyer");
+    document.getElementById("tasks").innerHTML += `
+    <div class="d-flex justify-content-between align-items-center mb-3 border-bottom">
+    <span class="my-0 taskname">-${x}</span>
+    <button class="btn btn-danger delete">Delete</button>
+    </div>
+    `;
+    // delete task
+    let taskDelete = document.querySelectorAll(".delete");
+    for (let i = 0; i < taskDelete.length; i++) {
+      taskDelete[i].onclick = function () {
+        console.log("click delete");
+        this.parentNode.remove();
+      };
+    }
+    // task done
+    let taskDone = document.querySelectorAll(".taskname");
+    for (let i = 0; i < taskDone.length; i++) {
+      taskDone[i].onclick = function () {
+        console.log("task done");
+        this.classList.toggle("completed");
+      };
+    }
+    //
+  }
+};
